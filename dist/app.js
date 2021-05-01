579298dab46e8ed1afe510156746d9ed7283396b
Nolan Eckert <nolan.eckert@gmail.com>
2021-04-30 23:35:58 -0600

    Update tsconfig. Add server.

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+"use strict";
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('We are alive!');
+});
+app.listen(port, err => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(`server is listening on ${port}`);
+});
